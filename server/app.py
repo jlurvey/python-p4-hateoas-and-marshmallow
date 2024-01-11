4678c04dfe99e39dff384ea0f7b94034350d4248
Joseph Lurvey <joeylurvey@gmail.com>
2024-01-11 17:33:56 -0500

    Complete code-along

--- server/app.py
+++ server/app.py
@@ -15,6 +15,8 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+ma = Marshmallow(app)
+
 api = Api(app)
 
 class Index(Resource):
@@ -38,10 +40,10 @@ class Newsletters(Resource):
 
     def get(self):
         
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        newsletters = Newsletter.query.all()
 
         response = make_response(
-            response_dict_list,
+            newsletters_schema.dump(newsletters),
             200,
         )
 
@@ -57,7 +59,7 @@ class Newsletters(Resource):
         db.session.add(new_record)
         db.session.commit()
 
-        response_dict = new_record.to_dict()
+        response_dict = new_record.schema.dump
 
         response = make_response(
             response_dict,
@@ -72,7 +74,7 @@ class NewsletterByID(Resource):
 
     def get(self, id):
 
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+        response_dict = Newsletter.query.filter_by(id=id).first().schema.dump
 
         response = make_response(
             response_dict,
@@ -90,7 +92,7 @@ class NewsletterByID(Resource):
         db.session.add(record)
         db.session.commit()
 
-        response_dict = record.to_dict()
+        response_dict = record.schema.dump
 
         response = make_response(
             response_dict,
@@ -117,6 +119,26 @@ class NewsletterByID(Resource):
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
+class NewsletterSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Newsletter
+        load_instance = True
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
